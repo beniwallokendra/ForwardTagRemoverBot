c67331f2adc6f361abc581d75939a9b37ba649b3
beniwallokendra <64604119+beniwallokendra@users.noreply.github.com>
2021-07-05 10:32:22 +0545

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ import os
 class Config:
 	
  TOKEN=os.environ.get("BOT_TOKEN",None)
- SOURCE="https://github.com/Artis7eeR/ForwardTagRemoverBot"
- START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do \n ©Artis7eeR"
+ SOURCE="https://www.instagram.com/loki_meme_wallah/"
+ START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do \n ©beniwallokendra"
  HELP_TEXT="Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media"
 	
